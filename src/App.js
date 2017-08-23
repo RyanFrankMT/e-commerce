2adcf7e2a403e44dffbc83f5413ad9b96cbce717
Ryan Frank <ryanfrank@Ryans-MacBook-Pro.local>
2017-08-22 21:33:26 -0600

    started e-commerce/client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import DataProvider from './Containers/DataProvider/DataProvider'
 
 class App extends Component {
   render () {
     return (
       <div>
-        <h1>Create React App Boilerplate Installed</h1>
+        <DataProvider />
       </div>
     )
   }
